a95b9fe7e6b8db42d9edaead90fb7b3b2d29c0af
Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>
2024-08-31 19:13:34 +0800

    build(docker): update xcaddy build plugins and entrypoint script
    Also enabling DOCKER_BUILDKIT to slience legacy builder warnings
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>

--- docker/caddy/entrypoint.sh
+++ docker/caddy/entrypoint.sh
@@ -1,8 +1,18 @@
-#!/usr/bin sh
+#!/usr/bin/env bash
+set -xe
+TAILSCALE_AUTHKEY=${TAILSCALE_AUTHKEY:-""}
+TAILSCALE_HOSTNAME=${TAILSCALE_HOSTNAME:-"caddy"}
+TAILSCALE_UP_ADDITIONAL_ARGS=${TAILSCALE_UP_ADDITIONAL_ARGS:-""}
+TAILSCALE_TAGS="tag:caddy,tag:docker"
+CADDYFILE_PATH=${CADDYFILE_PATH:-"/etc/caddy/Caddyfile"}
 
-if [[ $TAILSCALE_AUTHKEY ]]; then
-  tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-  tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME:-"caddy"}
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+if [[ $TAILSCALE_AUTHKEY != "" ]]; then
+    tailscale up \
+        --authkey="${TAILSCALE_AUTHKEY}" \
+        --hostname=${TAILSCALE_HOSTNAME} \
+        --accept-tags=${TAILSCALE_TAGS} \
+        ${TAILSCALE_UP_ADDITIONAL_ARGS}
 fi
 
-caddy run --config ${CADDYFILE_PATH:-"/etc/caddy/Caddyfile"} --adapter caddyfile
+caddy run --config ${CADDYFILE_PATH}
